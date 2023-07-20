d6fbe94d1ed3bc5e9467ccac5ae10d19256d1e2a
John Alexander Quintero Berrío <jaquinterob@hotmail.com>
2023-07-19 21:47:05 -0500

    update packages

---
+++ src/test/java/com/templechecklist/TempleChecklistApplicationTests.java
@@ -0,0 +1,13 @@
+package com.templechecklist;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TempleChecklistApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
